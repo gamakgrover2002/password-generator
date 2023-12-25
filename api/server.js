386eff0e57a31e2f76a593a5dc33ea65657c4ce0
Gamak Grover <you@example.com>
2023-12-26 00:35:56 +0530

    ui update

--- api/server.js
+++ api/server.js
@@ -4,24 +4,35 @@ const generator = require("generate-password");
 const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
+
+var passwordlength = 10;
 var password = generator.generate({
-  length: 10,
+  length: passwordlength,
   numbers: true,
 });
 const generatepassword = () => {
   password = generator.generate({
-    length: 10,
+    length: passwordlength,
     numbers: true,
   });
 };
 app.use(cors());
 app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   generatepassword();
-  console.log(password);
   res.json(password);
 });
+app.post("/data", (req, res) => {
+  var { length } = req.body;
 
+  if (length < 0 || length > 20) {
+    res.json("length not possible");
+  } else {
+    passwordlength = length;
+    res.json("length changed to : " + length);
+  }
+});
 app.listen(4000, () => {
   console.log("connected");
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,69 @@
 import React from "react";
 import { useRef } from "react";
-import { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
+
 function App() {
   const [password, Setpassword] = useState("");
   const [arr, Setarr] = useState([]);
+  const [length, Setlength] = useState(10);
+
   const generated = useRef(0);
+
+  const senddata = async () => {
+    try {
+      var res = await fetch("http://localhost:4000/data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          length: length,
+        }),
+      });
+      res = await res.json();
+      console.log(res);
+      alert(res);
+    } catch (err) {
+      console.log("error");
+    }
+  };
+  const getLength = (e) => {
+    Setlength(e.target.value);
+  };
   const getdata = async () => {
     const res = await fetch("http://localhost:4000/");
     const result = await res.json();
-    console.log(result);
+
     Setpassword(result);
-    Setarr([...arr, password]);
-  };
-  useEffect(() => {
+    Setarr([password, ...arr]);
     generated.current = generated.current + 1;
-  });
+  };
+
   return (
     <>
-      <h1>{password}</h1>
-      <br />
-      <button onClick={getdata}>Click to generate Password</button>
-      <br />
-      <h4>Password generated: {generated.current}</h4>
-      <br />
       <div id="container">
+        <h1>{password}</h1>
+        <br />
+        <button id="button1" onClick={getdata}>
+          Click to generate Password
+        </button>
+        <br />
+        <input
+          value={length}
+          onChange={getLength}
+          type="number"
+          id="length-input"
+        ></input>
+        <br />
+        <button id="button2" onClick={senddata}>
+          Set Length
+        </button>
+        <br />
+        <h4>Password generated: {generated.current}</h4>
+        <br />
+      </div>
+      <div id="subcontainer">
         {arr.map((e) => {
           return (
             <div id="child">
